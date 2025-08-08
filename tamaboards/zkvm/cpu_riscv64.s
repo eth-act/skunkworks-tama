9745bc18b704b66b2f25e6a553054d8c4b4efe32
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2025-08-08 22:35:58 +0100

    clear argv/argc

--- tamaboards/zkvm/cpu_riscv64.s
+++ tamaboards/zkvm/cpu_riscv64.s
@@ -4,4 +4,8 @@
 
 // Entry point - this is where execution starts
 TEXT cpuinit(SB),NOSPLIT|NOFRAME,$0
+	// Clear A0/A1 registers so runtime does not think we have
+	// argc/argv arguments
+	MOV	$0, A0
+	MOV	$0, A1
 	JMP	_rt0_tamago_start(SB)
