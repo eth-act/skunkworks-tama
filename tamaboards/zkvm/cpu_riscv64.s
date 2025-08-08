5ea0563f207d314f8e08f746bcf8e1aff2f1cbfb
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2025-08-08 22:34:05 +0100

    simplify cpuinit

--- tamaboards/zkvm/cpu_riscv64.s
+++ tamaboards/zkvm/cpu_riscv64.s
@@ -1,18 +1,7 @@
-//go:build tamago && riscv64
+//go:build tamago && riscv64 && linkcpuinit
 
 #include "textflag.h"
 
 // Entry point - this is where execution starts
 TEXT cpuinit(SB),NOSPLIT|NOFRAME,$0
-	// Clear A0/A1 registers so runtime does not think we have
-	// argc/argv arguments
-	MOV	$0, A0
-	MOV	$0, A1
-	// Jump to tamago runtime for RISC-V
-	JMP	runtime·rt0_riscv64_tamago(SB)
-
-// hwinit0 is called by the runtime during initialization
-TEXT runtime·hwinit0(SB),NOSPLIT|NOFRAME,$0
-	// Hardware initialization before runtime setup
-	// For zkVM, nothing special needed here
-	RET
+	JMP	_rt0_tamago_start(SB)
