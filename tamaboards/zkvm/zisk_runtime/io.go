bf21842ac230c98015b5e628ee5b3bac5a05959d
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2025-08-10 00:09:31 +0100

    add runtime related methods

---
+++ tamaboards/zkvm/zisk_runtime/io.go
@@ -0,0 +1,103 @@
+//go:build tamago && riscv64
+
+package zisk_runtime
+
+import (
+	"tamagotest/tamaboards/zkvm"
+	"unsafe"
+)
+
+/*
+
+Note: This file only defines guest reads and writes(public inputs). It does not define host methods.
+
+*/
+
+// Input/Output pointers to track current read/write positions
+var (
+	// INPUT_PTR tracks the current read position in the input buffer
+	// Starts at 0, which maps to INPUT_ADDR + 16 (where actual data begins)
+	//
+	// Zisk stores input at INPUT_ADDR with this format:
+	// INPUT_ADDR + 0: 8 bytes - free input (reserved, currently 0)
+	// INPUT_ADDR + 8: 8 bytes - input length (of all inputs) -- We actually do not want to use this. We want the input length per item
+	// TODO: To be safe, we can take the total input length and ensure that we never read past it by comparing to INPUT_PTR
+	// INPUT_ADDR + 16: actual input data
+	INPUT_PTR uint64 = 0
+
+	// OUTPUT_PTR tracks the current write position in the output buffer
+	OUTPUT_PTR uint64 = 0
+)
+
+// // ReadUint64 reads the next uint64 from the input buffer and advances the pointer
+// func ReadUint64() uint64 {
+// 	ptr := (*uint64)(unsafe.Pointer(uintptr(INPUT_ADDR + 16 + INPUT_PTR)))
+// 	INPUT_PTR += 8
+// 	return *ptr
+// }
+
+// ReadBytes reads n bytes from the input buffer and advances the pointer
+// This method reads the bytes from the RAM, so its the most low level
+// function we have for reading input.
+//
+// We recommend users use the Read methods. This is "unsafe" because
+// it is possible to read halfway into a struct and cause subsequent reads
+// to be misaligned
+func UnsafeReadBytes(n int) []byte {
+	result := make([]byte, n)
+	src := unsafe.Pointer(uintptr(zkvm.INPUT_ADDR + 16 + INPUT_PTR))
+	dst := unsafe.Pointer(&result[0])
+	// Copy the bytes
+	for i := 0; i < n; i++ {
+		*(*byte)(unsafe.Add(dst, i)) = *(*byte)(unsafe.Add(src, i))
+	}
+	INPUT_PTR += uint64(n)
+	return result
+	/*
+		TODO: Try the following:
+		func UnsafeReadBytes(n int) []byte {
+		    result := make([]byte, n)
+		    src := unsafe.Pointer(uintptr(zkvm.INPUT_ADDR + 16 + INPUT_PTR))
+		    srcSlice := unsafe.Slice((*byte)(src), n)
+		    copy(result, srcSlice)
+		    INPUT_PTR += uint64(n)
+		    return result
+		}
+	*/
+}
+
+// // WriteUint64 writes a uint64 to the output buffer and advances the pointer
+// func WriteUint64(value uint64) {
+// 	ptr := (*uint64)(unsafe.Pointer(uintptr(OUTPUT_ADDR + OUTPUT_PTR)))
+// 	*ptr = value
+// 	OUTPUT_PTR += 8
+// }
+
+// CommitBytes writes bytes to the output buffer and advances the pointer
+//
+// Noite: This method is used for writing public inputs.
+// It is not used for sending data fromthe host to guest.
+// TODO: Think about function naming
+func CommitBytes(data []byte) {
+	dst := unsafe.Pointer(uintptr(zkvm.OUTPUT_ADDR + OUTPUT_PTR))
+	src := unsafe.Pointer(&data[0])
+	// Copy the bytes
+	for i := 0; i < len(data); i++ {
+		*(*byte)(unsafe.Add(dst, i)) = *(*byte)(unsafe.Add(src, i))
+	}
+	OUTPUT_PTR += uint64(len(data))
+
+	/*
+		TODO: Try the following:
+		func CommitBytes(data []byte) {
+		    if len(data) == 0 {
+		        return // Handle empty slice case
+		    }
+		    dst := unsafe.Pointer(uintptr(zkvm.OUTPUT_ADDR + OUTPUT_PTR))
+		    dstSlice := unsafe.Slice((*byte)(dst), len(data))
+		    copy(dstSlice, data)
+		    OUTPUT_PTR += uint64(len(data))
+		}
+	*/
+
+}
---
+++ tamaboards/zkvm/zisk_runtime/serde.go
@@ -0,0 +1,67 @@
+//go:build tamago && riscv64
+
+package zisk_runtime
+
+import (
+	"crypto/sha256"
+	"hash"
+	"tamagotest/tamaboards/serialization"
+)
+
+// publicInputs will store a hash of all public inputs
+// This is an optimization, where instead of making all public inputs
+// public, we make a hash of the public inputs publish instead.
+// TODO: This is curently unused. See `Commit`
+var publicInputsHash hash.Hash = sha256.New()
+
+// Read deserializes any supported type from the input buffer
+// The first 64 bits specify the length of the serialized data
+//
+// Usage examples:
+//
+//	x := Read[int64]()           // Read an int64
+//	s := Read[string]()          // Read a string
+//	p := Read[Person]()          // Read a struct
+//	data := Read[[]byte]()       // Read a byte slice
+func Read[T any]() T {
+	// Read the length first (8 bytes)
+	// TODO: Note that this wastes bytes if we are serializing small structures like u8
+	// TODO: since that will take up 72 bits. We can optimize this later.
+	lengthBytes := UnsafeReadBytes(8)
+	length := uint64(0)
+	for i := 0; i < 8; i++ {
+		length |= uint64(lengthBytes[i]) << (i * 8)
+	}
+
+	// Read the serialized data of the specified length
+	data := UnsafeReadBytes(int(length))
+
+	// Deserialize it into the result
+	var result T
+	serialization.DeserializeData(data, &result)
+
+	return result
+}
+
+// Commit serializes any supported type and commits it as a public input/output
+//
+// Note: The public inputs optimization would modify the below to only call
+// CommitBytes on the hash.
+//
+// Usage examples:
+//
+//	Commit(result)               // Commit a computation result
+//	Commit(hash)                 // Commit a hash value
+//	Commit(publicKey)            // Commit a public key
+func Commit[T any](value T) {
+	// Serialize the value
+	data := serialization.MustSerializeData(value)
+
+	// TODO: Hash all public inputs and make this commit a hash instead
+	// TODO: This can be hidden from the verifier by making a PublicInputs struct
+	// TODO: That takes all of the public inputs in and does the hashing or something to that nature
+	// TODO: Eg: VerifyProof(proof : []byte, publicInput : PublicInput) -> bool
+
+	// Commit the serialized bytes
+	CommitBytes(data)
+}
