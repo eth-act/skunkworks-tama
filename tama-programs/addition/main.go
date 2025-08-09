aa28f0c7cd263b834fb0169a3bf6b9869f74a337
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2025-08-10 00:12:52 +0100

    add addition program

---
+++ tama-programs/addition/main.go
@@ -0,0 +1,23 @@
+//go:build tamago && riscv64
+
+package main
+
+import (
+	"fmt"
+	"tamagotest/tamaboards/zkvm/zisk_runtime"
+)
+
+func main() {	
+	// Read two integers using the generic Read function
+	x := zisk_runtime.Read[int64]()
+	y := zisk_runtime.Read[int64]()
+	
+	// Calculate sum
+	sum := x + y
+	
+	// Display result
+	fmt.Printf("Input: %d + %d = %d\n", x, y, sum)
+	
+	// Commit result as public output (no length prefix)
+	zisk_runtime.Commit(sum)
+}
