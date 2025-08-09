64905c92289777e3be1467a1441213a317d6d917
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2025-08-10 00:10:12 +0100

    go fmt

--- tamaboards/zkvm/board.go
+++ tamaboards/zkvm/board.go
@@ -5,21 +5,23 @@ package zkvm
 import (
 	"runtime"
 	"unsafe"
+
+	// TODO: can we remove this import and just define hwinit0
 	_ "github.com/usbarmory/tamago/riscv64"
 )
 
 const (
 	// ZisK ROM addresses (matching Zisk memory map from mem.rs)
-	ROM_ENTRY = 0x1000      // First BIOS instruction address
-	ROM_EXIT  = 0x1004      // Last BIOS instruction address
-	ROM_ADDR  = 0x80000000  // First program ROM instruction address
-	
+	ROM_ENTRY = 0x1000     // First BIOS instruction address
+	ROM_EXIT  = 0x1004     // Last BIOS instruction address
+	ROM_ADDR  = 0x80000000 // First program ROM instruction address
+
 	// ZisK I/O addresses (matching Zisk memory map from mem.rs)
-	INPUT_ADDR  = 0x90000000  // First input data memory address
-	SYS_ADDR    = 0xa0000000  // First system RW memory address (RAM_ADDR)
+	INPUT_ADDR = 0x90000000 // First input data memory address
+	SYS_ADDR   = 0xa0000000 // First system RW memory address (RAM_ADDR)
 	// TODO: Check: Can BSS be initialized at system address and UART_ADDR overwrite it
-	UART_ADDR   = 0xa0000200  // UART memory address (SYS_ADDR + 512)
-	OUTPUT_ADDR = 0xa0010000  // First output RW memory address
+	UART_ADDR   = 0xa0000200 // UART memory address (SYS_ADDR + 512)
+	OUTPUT_ADDR = 0xa0010000 // First output RW memory address
 )
 
 //go:linkname ramStart runtime.ramStart
@@ -29,21 +31,24 @@ var ramStart uint64 = 0xa0020000 // Match ZisK's AVAILABLE_MEM_ADDR
 var ramSize uint64 = 0x1FFE0000 // Match ZisK's RAM size (~512MB)
 
 // ramStackOffset is always defined here as there's no linkramstackoffset build tag
+//
 //go:linkname ramStackOffset runtime.ramStackOffset
 var ramStackOffset uint64 = 0x100000 // 1MB stack (matching linker script and ZisK)
 
 // TODO: We can probably remove this
-// Bloc sets the heap start address to bypass initBloc() 
+// Bloc sets the heap start address to bypass initBloc()
+//
 //go:linkname Bloc runtime.Bloc
 var Bloc uintptr = 0xa0120000 // Start heap after stack (ramStart + ramStackOffset)
 
 // printk implementation for zkVM
+//
 //go:linkname printk runtime.printk
 func printk(c byte) {
 	*(*byte)(unsafe.Pointer(uintptr(UART_ADDR))) = c
 }
 
-// hwinit1 is now defined in hwinit1.s 
+// hwinit1 is now defined in hwinit1.s
 // we use it to set A0/A1 registers to the input and output address
 
 // Use this as a stub timer. It is all single threaded, and there is no concept of time.
@@ -59,7 +64,8 @@ func nanotime1() int64 {
 	return timer * 1000
 }
 
-// Init initializes the zkVM 
+// Init initializes the zkVM
+//
 //go:linkname Init runtime.hwinit1
 func Init() {
 	// Set up custom exit handler
